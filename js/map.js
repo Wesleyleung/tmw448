fc4100b11bf7dd5cf62adba4d389b272c735a0cd
Wesley Leung <Wes.k.Leung@gmail.com>
2012-12-03 00:51:39 -0800

    basic heatmap

--- js/map.js
+++ js/map.js
@@ -1,40 +1,58 @@
 var map;
 
+var heatMapData = [
+  {location: new google.maps.LatLng(37.782, -122.447), weight: 0.5},
+  new google.maps.LatLng(37.782, -122.445),
+  {location: new google.maps.LatLng(37.782, -122.443), weight: 2},
+  {location: new google.maps.LatLng(37.782, -122.441), weight: 3},
+  {location: new google.maps.LatLng(37.782, -122.439), weight: 2},
+  new google.maps.LatLng(37.782, -122.437),
+  {location: new google.maps.LatLng(37.782, -122.435), weight: 0.5},
+
+  {location: new google.maps.LatLng(37.785, -122.447), weight: 3},
+  {location: new google.maps.LatLng(37.785, -122.445), weight: 2},
+  new google.maps.LatLng(37.785, -122.443),
+  {location: new google.maps.LatLng(37.785, -122.441), weight: 0.5},
+  new google.maps.LatLng(37.785, -122.439),
+  {location: new google.maps.LatLng(37.785, -122.437), weight: 2},
+  {location: new google.maps.LatLng(37.785, -122.435), weight: 3}
+];
+
 function initialize() {
-var mapOptions = {
-	zoom: 8,
-	mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-map = new google.maps.Map(document.getElementById('map_canvas'),
-		mapOptions);
+  var mapOptions = {
+    zoom: 8,
+  	mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById('map_canvas'),
+  		mapOptions);
 
-// Try HTML5 geolocation
-if(navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(function(position) {
-		var pos = new google.maps.LatLng(position.coords.latitude,
-																		 position.coords.longitude);
+  // Try HTML5 geolocation
+  if(navigator.geolocation) {
+  	navigator.geolocation.getCurrentPosition(function(position) {
+  		var pos = new google.maps.LatLng(position.coords.latitude,
+  																		 position.coords.longitude);
 
-		var infowindow = new google.maps.InfoWindow({
-			map: map,
-			position: pos,
-			content: 'Location found using HTML5.'
-		});
+  		var infowindow = new google.maps.InfoWindow({
+  			map: map,
+  			position: pos,
+  			content: 'Location found using HTML5.'
+  		});
 
-		map.setCenter(pos);
-	}, function() {
-		handleNoGeolocation(true);
-	});
-} else {
-	// Browser doesn't support Geolocation
-	handleNoGeolocation(false);
-}
+  		map.setCenter(pos);
+  	}, function() {
+  		handleNoGeolocation(true);
+  	});
+  } else {
+  	// Browser doesn't support Geolocation
+  	handleNoGeolocation(false);
+  }
 }
 
 function handleNoGeolocation(errorFlag) {
-if (errorFlag) {
-	var content = 'Error: The Geolocation service failed.';
-} else {
-	var content = 'Error: Your browser doesn\'t support geolocation.';
+  if (errorFlag) {
+  	var content = 'Error: The Geolocation service failed.';
+  } else {
+  	var content = 'Error: Your browser doesn\'t support geolocation.';
 }
 
 var options = {
@@ -45,6 +63,12 @@ var options = {
 
 var infowindow = new google.maps.InfoWindow(options);
 map.setCenter(options.position);
+
+var heatmap = new google.maps.visualization.HeatmapLayer({
+  data: heatMapData
+});
+heatmap.setMap(map);
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
