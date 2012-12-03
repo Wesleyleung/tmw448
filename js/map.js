ef1d325258d93ba181313f7b935c8b95b93f4e4c
Tim Shi <timgshi@gmail.com>
2012-12-03 01:22:49 -0800

    some json stuff

--- js/map.js
+++ js/map.js
@@ -1,22 +1,22 @@
 var map;
 
-var heatMapData = [
-  {location: new google.maps.LatLng(37.782, -122.447), weight: 0.5},
-  new google.maps.LatLng(37.782, -122.445),
-  {location: new google.maps.LatLng(37.782, -122.443), weight: 2},
-  {location: new google.maps.LatLng(37.782, -122.441), weight: 3},
-  {location: new google.maps.LatLng(37.782, -122.439), weight: 2},
-  new google.maps.LatLng(37.782, -122.437),
-  {location: new google.maps.LatLng(37.782, -122.435), weight: 0.5},
+// var heatMapData = [
+//   {location: new google.maps.LatLng(37.782, -122.447), weight: 0.5},
+//   new google.maps.LatLng(37.782, -122.445),
+//   {location: new google.maps.LatLng(37.782, -122.443), weight: 2},
+//   {location: new google.maps.LatLng(37.782, -122.441), weight: 3},
+//   {location: new google.maps.LatLng(37.782, -122.439), weight: 2},
+//   new google.maps.LatLng(37.782, -122.437),
+//   {location: new google.maps.LatLng(37.782, -122.435), weight: 0.5},
 
-  {location: new google.maps.LatLng(37.785, -122.447), weight: 3},
-  {location: new google.maps.LatLng(37.785, -122.445), weight: 2},
-  new google.maps.LatLng(37.785, -122.443),
-  {location: new google.maps.LatLng(37.785, -122.441), weight: 0.5},
-  new google.maps.LatLng(37.785, -122.439),
-  {location: new google.maps.LatLng(37.785, -122.437), weight: 2},
-  {location: new google.maps.LatLng(37.785, -122.435), weight: 3}
-];
+//   {location: new google.maps.LatLng(37.785, -122.447), weight: 3},
+//   {location: new google.maps.LatLng(37.785, -122.445), weight: 2},
+//   new google.maps.LatLng(37.785, -122.443),
+//   {location: new google.maps.LatLng(37.785, -122.441), weight: 0.5},
+//   new google.maps.LatLng(37.785, -122.439),
+//   {location: new google.maps.LatLng(37.785, -122.437), weight: 2},
+//   {location: new google.maps.LatLng(37.785, -122.435), weight: 3}
+// ];
 
 function initialize() {
   var mapOptions = {
@@ -64,10 +64,25 @@ var options = {
 var infowindow = new google.maps.InfoWindow(options);
 map.setCenter(options.position);
 
-var heatmap = new google.maps.visualization.HeatmapLayer({
-  data: heatMapData
+console.log("before function");
+
+$.getJSON('js/locations.json', function(json) {
+	console.log("in function");
+	// console.log(json);
+	// var heatMapData = [];
+	// for (var i = 0; i < json.locations.length; i++) {
+	// 	var location = json.locations[i];
+	// 	var dict = {location: new google.maps.LatLng(location.coords.lat, location.coords.lng), weight: location.weight};
+	// 	heatMapData.push(dict);
+	// };
+	// console.log(heatMapData);
+	// var heatmap = new google.maps.visualization.HeatmapLayer({
+	//   data: heatMapData
+	// });
+	// heatmap.setMap(map);
 });
-heatmap.setMap(map);
+
+console.log("after function");
 
 }
 
