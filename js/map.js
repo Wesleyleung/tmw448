ff94e7f26c709038074dd4f0239e07b71a327995
Wesley Leung <Wes.k.Leung@gmail.com>
2012-12-03 21:21:50 -0800

    fixed json parsing

--- js/map.js
+++ js/map.js
@@ -33,85 +33,69 @@ var heatMapData = {locations : [{coords: {lat: 37.782, lng: -122.447}, weight: 0
 function initialize() {
   var mapOptions = {
     zoom: 8,
+    center: new google.maps.LatLng(37.424106,-122.166076),  //uncomment this later
   	mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   map = new google.maps.Map(document.getElementById('map_canvas'),
   		mapOptions);
 
   // Try HTML5 geolocation
-  if(navigator.geolocation) {
-  	navigator.geolocation.getCurrentPosition(function(position) {
-  		var pos = new google.maps.LatLng(position.coords.latitude,
-  																		 position.coords.longitude);
+  // if(navigator.geolocation) {
+  // 	navigator.geolocation.getCurrentPosition(function(position) {
+  // 		var pos = new google.maps.LatLng(position.coords.latitude,
+  // 																		 position.coords.longitude);
 
-  		var infowindow = new google.maps.InfoWindow({
-  			map: map,
-  			position: pos,
-  			content: 'Location found using HTML5.'
-  		});
+  // 		var infowindow = new google.maps.InfoWindow({
+  // 			map: map,
+  // 			position: pos,
+  // 			content: 'Location found using HTML5.'
+  // 		});
 
-  		map.setCenter(pos);
-  	}, function() {
-  		handleNoGeolocation(true);
-  	});
-  } else {
-  	// Browser doesn't support Geolocation
-  	handleNoGeolocation(false);
-  }
+  // 		map.setCenter(pos);
+  // 	}, function() {
+  // 		handleNoGeolocation(true);
+  // 	});
+  // } else {
+  // 	// Browser doesn't support Geolocation
+  // 	handleNoGeolocation(false);
+  // }
+
+  parseJSONObj();
 }
 
 function handleNoGeolocation(errorFlag) {
   if (errorFlag) {
   	var content = 'Error: The Geolocation service failed.';
   } else {
-  	var content = 'Error: Your browser doesn\'t support geolocation.';
+  		var content = 'Error: Your browser doesn\'t support geolocation.';
+	}
+
+	var options = {
+		map: map,
+		position: new google.maps.LatLng(37.424106,-122.166076),
+		content: content
+	};
+
+	var infowindow = new google.maps.InfoWindow(options);
+	map.setCenter(options.position);
 }
 
-var options = {
-	map: map,
-	position: new google.maps.LatLng(37.424106,-122.166076),
-	content: content
-};
-
-var infowindow = new google.maps.InfoWindow(options);
-map.setCenter(options.position);
-
-// console.log("before function");
-
-// $.getJSON('js/locations.json', function(json) {
-// 	console.log("in function");
-	// console.log(json);
-	// var heatMapData = [];
-	// for (var i = 0; i < json.locations.length; i++) {
-	// 	var location = json.locations[i];
-	// 	var dict = {location: new google.maps.LatLng(location.coords.lat, location.coords.lng), weight: location.weight};
-	// 	heatMapData.push(dict);
-	// };
-	// console.log(heatMapData);
-	// var heatmap = new google.maps.visualization.HeatmapLayer({
-	//   data: heatMapData
-	// });
-	// heatmap.setMap(map);
-// });
-
-var dataArray = [];
-for (var i = 0; i < heatMapData.locations.length; i++) {
-	var location = heatMapData.locations[i];
-	var dict = {location: new google.maps.LatLng(location.coords.lat, location.coords.lng), weight: location.weight};
-	dataArray.push(dict);
-};
-console.log(heatMapData);
-var heatmap = new google.maps.visualization.HeatmapLayer({
-  data: dataArray
-});
-heatmap.setOptions({
-	opacity:0.5,
-	radius:20
-});
-heatmap.setMap(map);
-
-// console.log("after function");
-
+function parseJSONObj() {
+	$.getJSON("js/locations.json", function(json) {
+		console.log(json);
+		var heatMapData = [];
+		for (var i = 0; i < json.locations.length; i++) {
+			var location = json.locations[i];
+			var dict = {location: new google.maps.LatLng(location.coords.lat, location.coords.lng), weight: location.weight};
+			heatMapData.push(dict);
+		};
+		var heatmap = new google.maps.visualization.HeatmapLayer({
+		  data: heatMapData
+		});
+		 heatmap.setOptions({
+		 	opacity:0.5,
+	 		radius:20
+		 });
+		heatmap.setMap(map);
+	 });
 }
-
-google.maps.event.addDomListener(window, 'load', initialize);
