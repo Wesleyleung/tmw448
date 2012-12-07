97628d16c8e704700bcc91f9151cfa5c318b4266
Tim Shi <timgshi@gmail.com>
2012-12-07 01:01:03 -0800

    fixed s3 prefix further

--- nikeplusstanford/settings/prod.py
+++ nikeplusstanford/settings/prod.py
@@ -103,7 +103,7 @@ AWS_HEADERS = {
 }
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = 'https://%s.s3.amazonaws.com/%s/' % (AWS_STORAGE_BUCKET_PREFIX, AWS_STORAGE_BUCKET_NAME)
+STATIC_URL = 'https://%s.amazonaws.com/%s/' % (AWS_STORAGE_BUCKET_PREFIX, AWS_STORAGE_BUCKET_NAME)
 ########## END STORAGE CONFIGURATION
 
 
