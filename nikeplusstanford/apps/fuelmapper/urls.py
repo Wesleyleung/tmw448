049ab9aeab49f15d1b5c4647169daf726d55a937
Tim Shi <timgshi@gmail.com>
2012-12-07 01:52:04 -0800

    added json method

--- nikeplusstanford/apps/fuelmapper/urls.py
+++ nikeplusstanford/apps/fuelmapper/urls.py
@@ -4,4 +4,5 @@ from apps.fuelmapper import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name="index"),
+	url(r'^loadStaticJSON$', views.loadStaticJSON, name="loadStaticJSON"),
 )
--- nikeplusstanford/apps/fuelmapper/views.py
+++ nikeplusstanford/apps/fuelmapper/views.py
@@ -1,7 +1,21 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.conf import settings
 import json
+import httplib2
+from os import environ
 
 def index(request):
 	context = {}
 	return render(request, 'fuelmapper/index.html', context)
+
+def loadStaticJSON(request):
+	json_file = request.GET['json_file']
+	if environ.get('HEROKU') is 'yes':
+		h = httplib2.Http()
+		request_url = settings.STATIC_ROOT + '/js/%s' % json_file
+		response, content = h.request(request_url)
+		return HttpResponse(content, mimetype='application/json')
+	else:
+		content = open(settings.STATIC_ROOT + '/js/%s' % json_file)
+		return HttpResponse(content.read(), mimetype='application/json')
