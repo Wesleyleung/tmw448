03933bec8c0f88fa7c526e82f9ebbc4e94a6c557
Tim Shi <timgshi@gmail.com>
2012-12-09 01:55:11 -0800

    postal code model created. csvs can be imported

--- nikeplusstanford/apps/fuelmapper/dataimporter.py
+++ nikeplusstanford/apps/fuelmapper/dataimporter.py
@@ -1,9 +1,24 @@
 import csv
 from apps.fuelmapper.models import NikeUser, NikeSportActivity
+from datetime import datetime
+
+def oracleTimeToDateTime(timeString):
+	'07-APR-12 12.00.00.000000000 AM'
+	monthName = timeString[3:6]
+	months = {'JAN' : '01', 'FEB' : '02', 'MAR' : '03', 'APR' : '04', 'MAY' : '05', 'JUN' : '06', 'JUL' : '07', 'AUG' : '08', 'SEP' : '09', 'OCT' : '10', 'NOV' : '11', 'DEC' : '12'}
+	fixed = timeString[:3] + months[monthName] + timeString[6:18] + timeString[-3:]
+	returnTime = datetime.strptime(fixed, '%d-%m-%y %I.%M.%S %p')
+	return returnTime
 
 def importFromCSV(filepath):
-	csv_file = open(filepath, 'rb')
-	for line in csv.DictReader(csv_file, delimiter=','):
+	csvDict = csv.DictReader(open(filepath, 'rU'))
+
+	csvDict.fieldnames = [field.strip().lower() for field in csvDict.fieldnames]
+
+	for line in csvDict:
+		newTime = oracleTimeToDateTime(line['start_time_local'])
+		line['start_time_local'] = newTime
 		print line
 		newActivity = NikeSportActivity(**line)
+		newActivity.save()
 		print newActivity
--- nikeplusstanford/apps/fuelmapper/models.py
+++ nikeplusstanford/apps/fuelmapper/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import httplib2
+import json
 
 class NikeUser(models.Model):
     nike_id = models.CharField(max_length=200, unique=True)
@@ -37,7 +39,7 @@ class NikeSportActivity(models.Model):
 	sport_activity_id = models.CharField(max_length=40, unique=True)
 	upm_user_id = models.CharField(max_length=40)
 	nike_plus_user_id = models.CharField(max_length=40)
-	nike_user = models.ForeignKey(NikeUser, null=True, on_delete=models.SET_NULL)
+	nike_user = models.ForeignKey(NikeUser, null=True, on_delete=models.SET_NULL, default=None)
 	activity_type_id = models.IntegerField()
 	tz_offset = models.CharField(max_length=20)
 	start_time_local = models.DateTimeField()
@@ -56,5 +58,40 @@ class NikeSportActivity(models.Model):
 		return [(field.name, field.value_to_string(self)) for field in NikeSportActivity._meta.fields]
 
 	def __unicode__(self):
-		return str(self.get_fields())
+		return str(self.sport_activity_id)
+
+class PostalCode(models.Model):
+	postalcode = models.CharField(max_length=24, unique=True)
+	country = models.CharField(max_length=40, blank=True, default='')
+	lat = models.FloatField(blank=True, null=True, default=0)
+	lng = models.FloatField(blank=True, null=True, default=0)
+	northeast_lat = models.FloatField(blank=True, null=True, default=0)
+	northeast_lng = models.FloatField(blank=True, null=True, default=0)
+	southwest_lat = models.FloatField(blank=True, null=True, default=0)
+	southwest_lng = models.FloatField(blank=True, null=True, default=0)
+
+	@classmethod
+	def find_or_create_code(cls, postalcode):
+		obj, created = cls.objects.get_or_create(postalcode=postalcode)
+		if created is True:
+			h = httplib2.Http()
+			request_url = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false' % postalcode
+			response, content = h.request(request_url)
+			jsonResponse = json.loads(content)
+			print content
+			print jsonResponse
+			geometry = jsonResponse['results'][0]['geometry']
+			obj.postalcode = postalcode
+			print geometry
+			obj.lat = geometry['location']['lat']
+			obj.lng = geometry['location']['lng']
+			obj.northeast_lat = geometry['viewport']['northeast']['lat']
+			obj.northeast_lng = geometry['viewport']['northeast']['lng']
+			obj.southwest_lat = geometry['viewport']['southwest']['lat']
+			obj.southwest_lng = geometry['viewport']['southwest']['lng']
+			obj.save()
+		return obj
+
+	def __unicode__(self):
+		return '%s: location %f %f' % (self.postalcode, self.lat, self.lng)
 
