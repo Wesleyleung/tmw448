7403fde5e0cdca2a132a6bb8a10285b316108f44
Tim Shi <timgshi@gmail.com>
2012-12-12 17:15:50 -0800

    url for zip info

--- nikeplusstanford/apps/fuelmapper/urls.py
+++ nikeplusstanford/apps/fuelmapper/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
 	url(r'^$', views.index, name="index"),
 	url(r'^loadStaticJSON$', views.loadStaticJSON, name="loadStaticJSON"),
 	url(r'^loadSportFromZipcodeViewJSON$', views.loadSportFromZipcodeViewJSON, name="loadSportFromZipcodeViewJSON"),
+	url(r'^zip_info$', views.zip_info, name="zip_info"),
 )
